7a4f3c84971d159b2a9f89dbc5e26adb56998a19
ediakpa <eakpa001@ucr.edu>
2022-12-30 12:57:20 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// alert('Hello World');
+
+let height = Number(5) + "'" + Number(5) + '"';
+
+function myfunction(name, height, country) {
+    alert(name + ' ' + height + ' ' + country);
+    console.log(name + ' ' + height + ' ' + country);
+}
+
+myfunction('Edima', height, 'USA');
+
+
